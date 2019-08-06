d04bc78c447ad01a4774c2b753624a1a5961be07
Cameron Browning <camb416@gmail.com>
2019-08-06 18:55:21 -0400

    added gui control

--- tools/gradient-player/src/App.java
+++ tools/gradient-player/src/App.java
@@ -1,8 +1,18 @@
+import controlP5.ControlP5;
+import controlP5.Slider;
+import controlP5.Toggle;
 import processing.core.PApplet;
 import processing.core.PGraphics;
 import processing.core.PImage;
 
 public class App extends PApplet {
+
+    ControlP5 cp5;
+    Slider gPosSlider;
+    Toggle gAutoScrollToggle;
+    Slider gSpeedSlider;
+    Toggle bUseLights;
+    Toggle bUseStroke;
 
     PImage img;
     int imgWidth;
@@ -46,13 +56,50 @@ public class App extends PApplet {
 
         swatchSize = (imgWidth - margin * 3) / 4;
 
+        cp5 = new ControlP5(this);
+        gPosSlider = cp5.addSlider("v1")
+                .setPosition(margin, imgHeight + margin * 3 + swatchSize)
+                .setSize(320, (swatchSize - margin) / 2)
+                .setRange(0, 1.0f)
+                .setValue(0.5f)
+                .setColorCaptionLabel(color(20, 20, 20));
+
+        gAutoScrollToggle = cp5.addToggle("auto")
+                .setPosition(margin, gPosSlider.getPosition()[1] + (swatchSize - margin) / 2 + margin)
+                .setSize(160 - margin, (swatchSize - margin) / 2);
+
+        bUseLights = cp5.addToggle("lights")
+                .setPosition(margin, gPosSlider.getPosition()[1] + (swatchSize - margin) / 2 * 2 + margin * 4)
+                .setSize(160 - margin, (swatchSize - margin) / 2);
+
+        gSpeedSlider = cp5.addSlider("speed")
+                .setPosition(margin * 2 + 160, gPosSlider.getPosition()[1] + (swatchSize - margin) / 2 + margin)
+                .setSize(140 - margin, (swatchSize - margin) / 2)
+                .setValue(0.0002f)
+                .setRange(0.0f, 0.002f);
+
+        bUseStroke = cp5.addToggle("stroke")
+                .setPosition(margin * 2 + 160, gPosSlider.getPosition()[1] + (swatchSize - margin) / 2 * 2 + margin * 4)
+                .setSize(160 - margin, (swatchSize - margin) / 2);
+
 
     }
 
-    public void update() {
-        gradientPosition += gradientAdder;
-        if (gradientPosition > 1.0f) gradientPosition = 0.0f;
+    public void keyPressed() {
+        gAutoScrollToggle.toggle();
+    }
 
+    public void update() {
+        if (gAutoScrollToggle.getBooleanValue()) {
+            gradientPosition += gSpeedSlider.getValue();
+            gPosSlider.setValue(gradientPosition);
+            if (gradientPosition > 1.0f) gradientPosition = 0.0f;
+        } else {
+            gradientPosition = gPosSlider.getValue();
+        }
+
+
+        if (bUseLights.getBooleanValue()) lights();
 
     }
 
@@ -68,21 +115,27 @@ public class App extends PApplet {
         colorC = buff.get((int) (gradientPosition * img.width), (int) ((68 + 2 * 100)));
         colorD = buff.get((int) (gradientPosition * img.width), (int) ((68 + 3 * 100)));
 
+        fill(255);
         image(buff, margin, margin, imgWidth, imgHeight);
-        stroke(255);
-        noFill();
-        rect(margin, margin, imgWidth, imgHeight);
+        //stroke(255);
+        //noFill();
+        //rect(margin, margin, imgWidth, imgHeight);
 
+        fill(255);
         stroke(255);
-        noFill();
-        fill(colorA);
-        rect((int) (gradientPosition * 320.0f), (int) ((68 + 0 * 100) / 4.5f), 5, 5);
-        fill(colorB);
-        rect((int) (gradientPosition * 320.0f), (int) ((68 + 1 * 100) / 4.5f), 5, 5);
-        fill(colorC);
-        rect((int) (gradientPosition * 320.0f), (int) ((68 + 2 * 100) / 4.5f), 5, 5);
-        fill(colorD);
-        rect((int) (gradientPosition * 320.0f), (int) ((68 + 3 * 100) / 4.5f), 5, 5);
+        //noFill();
+        //fill(colorA);
+        //point((int) (gradientPosition * 320.0f), (int) ((68 + 0 * 100) / 4.5f));
+        rect((int) (gradientPosition * 320.0f), (int) ((68 + 0 * 100) / 4.5f) + 5, 2, 2);
+        //fill(colorB);
+        rect((int) (gradientPosition * 320.0f), (int) ((68 + 1 * 100) / 4.5f) + 5, 2, 2);
+        //point((int) (gradientPosition * 320.0f), (int) ((68 + 1 * 100) / 4.5f));
+        //fill(colorC);
+        rect((int) (gradientPosition * 320.0f), (int) ((68 + 2 * 100) / 4.5f) + 5, 2, 2);
+        //point((int) (gradientPosition * 320.0f), (int) ((68 + 2 * 100) / 4.5f));
+        //fill(colorD);
+        rect((int) (gradientPosition * 320.0f), (int) ((68 + 3 * 100) / 4.5f) + 5, 2, 2);
+        //point((int) (gradientPosition * 320.0f), (int) ((68 + 3 * 100) / 4.5f));
 
         fill(colorA);
         rect(margin, margin * 2 + imgHeight, swatchSize, swatchSize);
@@ -114,7 +167,12 @@ public class App extends PApplet {
 
         scale(120);
         beginShape(QUADS);
-        noStroke();
+        if (bUseStroke.getBooleanValue()) {
+            strokeWeight(1.0f/120.0f);
+            stroke(255);
+        } else {
+            noStroke();
+        }
 
         fill(colorA);
         vertex(-1, 1, 1);
@@ -173,6 +231,7 @@ public class App extends PApplet {
         endShape();
 
         popMatrix();
+
 
     }
 
