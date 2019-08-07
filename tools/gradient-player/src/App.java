044600e837e712b5618ed681e5ac4b23fe5dce0e
Cameron Browning <camb416@gmail.com>
2019-08-07 16:39:39 -0400

    fully abstracted and cleaned up the color monager

--- tools/gradient-player/src/App.java
+++ tools/gradient-player/src/App.java
@@ -1,36 +1,12 @@
-import controlP5.ControlP5;
-import controlP5.Slider;
-import controlP5.Toggle;
 import processing.core.PApplet;
-import processing.core.PGraphics;
-import processing.core.PImage;
 
 public class App extends PApplet {
 
-    ControlP5 cp5;
-    Slider gPosSlider;
-    Toggle gAutoScrollToggle;
-    Slider gSpeedSlider;
-    Toggle bUseLights;
-    Toggle bUseStroke;
-
     Boolean isColorsVisible;
 
+    Boolean bUseStroke;
+
     ColorManager cm;
-
-    PImage img;
-    int imgWidth;
-    int imgHeight;
-    int margin = 5;
-
-    // float gradientPosition = 0.0f;
-
-    PGraphics buff;
-
-    int numColors;
-    int[] colors;
-
-
 
     float xmag, ymag = 0;
     float newXmag, newYmag = 0;
@@ -47,83 +23,36 @@ public class App extends PApplet {
 
     public void setup() {
 
+        bUseStroke = false;
         isColorsVisible = true;
 
         cm = new ColorManager(this, 8,"tools/gradient-player/colors.png");
-
-
-
-        int uiy = 360;
-        int swatchSize = cm.getSwatchSize();
-
-        cp5 = new ControlP5(this);
-        gPosSlider = cp5.addSlider("v1")
-                .setPosition(margin, uiy)
-                .setSize(320, (swatchSize - margin) / 2)
-                .setRange(0, 1.0f)
-                .setValue(0.5f)
-                .setColorCaptionLabel(color(20, 20, 20));
-
-        gAutoScrollToggle = cp5.addToggle("auto")
-                .setPosition(margin, gPosSlider.getPosition()[1] + (swatchSize - margin) / 2 + margin)
-                .setSize(160 - margin, (swatchSize - margin) / 2);
-
-        bUseLights = cp5.addToggle("lights")
-                .setPosition(margin, gPosSlider.getPosition()[1] + (swatchSize - margin) / 2 * 2 + margin * 4)
-                .setSize(160 - margin, (swatchSize - margin) / 2);
-
-        gSpeedSlider = cp5.addSlider("speed")
-                .setPosition(margin * 2 + 160, gPosSlider.getPosition()[1] + (swatchSize - margin) / 2 + margin)
-                .setSize(130 - margin, (swatchSize - margin) / 2)
-                .setValue(0.0002f)
-                .setRange(0.0f, 0.002f);
-
-        bUseStroke = cp5.addToggle("stroke")
-                .setPosition(margin * 2 + 160, gPosSlider.getPosition()[1] + (swatchSize - margin) / 2 * 2 + margin * 4)
-                .setSize(160 - margin, (swatchSize - margin) / 2);
-
 
     }
 
     public void keyPressed() {
         switch (key) {
             case ' ':
-                gAutoScrollToggle.toggle();
+                cm.playPause();
                 break;
             case 'd':
             case 'D':
-                cp5.setVisible(!cp5.isVisible());
-                isColorsVisible = !isColorsVisible;
-
+                cm.toggleVisibility();
                 break;
         }
 
     }
 
     public void update() {
-        if (gAutoScrollToggle.getBooleanValue()) {
-            cm.setGradientPosition(cm.getGradientPosition() + gSpeedSlider.getValue());
-            gPosSlider.setValue(cm.getGradientPosition());
-
-        } else {
-            cm.setGradientPosition(gPosSlider.getValue());
-        }
-
-
-        if (bUseLights.getBooleanValue()) lights();
 
     }
 
     public void draw() {
         update();
 
-
         background(0);
 
-
-
         if (isColorsVisible) {
-
 
             cm.draw();
 
@@ -150,7 +79,7 @@ public class App extends PApplet {
 
         scale(120);
         beginShape(QUADS);
-        if (bUseStroke.getBooleanValue()) {
+        if (bUseStroke) {
             strokeWeight(1.0f / 120.0f);
             stroke(255);
         } else {
@@ -214,7 +143,6 @@ public class App extends PApplet {
         endShape();
 
         popMatrix();
-
 
     }
 
--- tools/gradient-player/src/ColorManager.java
+++ tools/gradient-player/src/ColorManager.java
@@ -1,3 +1,6 @@
+import controlP5.ControlP5;
+import controlP5.Slider;
+import controlP5.Toggle;
 import processing.core.PApplet;
 import processing.core.PGraphics;
 import processing.core.PImage;
@@ -21,8 +24,13 @@ public class ColorManager {
 
     int swatchSize;
 
+    ControlP5 cp5;
+    Slider gPosSlider;
+    Toggle gAutoScrollToggle;
+    Slider gSpeedSlider;
 
-    public ColorManager(PApplet _app, int _numColors, String _filePath){
+
+    public ColorManager(PApplet _app, int _numColors, String _filePath) {
 
         margin = 5;
         swatchSize = (int) ((320 - 3 * margin) / 4.0f);
@@ -30,75 +38,128 @@ public class ColorManager {
 
         app = _app;
         filePath = _filePath;
-        numColors =  _numColors;
+        numColors = _numColors;
 
         numColors = 8;
         colors = new int[numColors];
 
-        buff = app.createGraphics(320+10, imgHeight);
+        buff = app.createGraphics(320 + 10, imgHeight);
         img = app.loadImage(filePath);
         imgWidth = 320;
         imgHeight = (int) (((float) img.height / (float) img.width) * 320.0f);
         buff = app.createGraphics(img.width, img.height);
 
-        cui = app.createGraphics(320,640);
+        cui = app.createGraphics(320, 640);
 
         buff.beginDraw();
         buff.image(img, 0, 0);
         buff.endDraw();
 
+        cp5 = new ControlP5(app);
+
+        isVisible = false;
+
+        int uiy = 360;
+
+        gPosSlider = cp5.addSlider("v1")
+                .setPosition(margin, uiy)
+                .setSize(320, (swatchSize - margin) / 2)
+                .setRange(0, 1.0f)
+                .setValue(0.5f)
+                .setColorCaptionLabel(app.color(20, 20, 20));
+
+        gAutoScrollToggle = cp5.addToggle("auto")
+                .setPosition(margin, gPosSlider.getPosition()[1] + (swatchSize - margin) / 2 + margin)
+                .setSize(160 - margin, (swatchSize - margin) / 2)
+                .setValue(true);
+
+        gSpeedSlider = cp5.addSlider("speed")
+                .setPosition(margin * 2 + 160, gPosSlider.getPosition()[1] + (swatchSize - margin) / 2 + margin)
+                .setSize(130 - margin, (swatchSize - margin) / 2)
+                .setValue(0.0002f)
+                .setRange(0.0f, 0.002f);
+
+        cp5.hide();
+
     }
 
-    public void setup(){
-        isVisible = false;
+    public void setup() {
+        //  isVisible = false;
     }
-    public void update(){
+
+    public void update() {
         if (gradientPosition > 1.0f) gradientPosition = 0.0f;
 
         for (int i = 0; i < numColors; i++) {
             colors[i] = buff.get((int) (gradientPosition * img.width), (int) ((68 + i * 100)));
         }
+
+        if (gAutoScrollToggle.getBooleanValue()) {
+            setGradientPosition(getGradientPosition() + gSpeedSlider.getValue());
+            gPosSlider.setValue(getGradientPosition());
+
+        } else {
+            setGradientPosition(gPosSlider.getValue());
+        }
+
     }
-    public void draw(){
+
+    public void draw() {
 
         this.update();
-        cui.beginDraw();
-        cui.fill(255);
-        cui.image(buff, margin, margin, imgWidth, imgHeight);
+        if (isVisible) {
 
-        cui.fill(255);
-        cui.stroke(255);
 
-        for (int i = 0; i < numColors; i++) {
-            cui.rect((int) (gradientPosition * 320.0f), (int) ((68 + i * 100) / 4.5f) + 5, 2, 2);
+            cui.beginDraw();
+            cui.fill(255);
+            cui.image(buff, margin, margin, imgWidth, imgHeight);
+
+            cui.fill(255);
+            cui.stroke(255);
+
+            for (int i = 0; i < numColors; i++) {
+                cui.rect((int) (gradientPosition * 320.0f), (int) ((68 + i * 100) / 4.5f) + 5, 2, 2);
+            }
+
+            for (int i = 0; i < numColors; i++) {
+                cui.fill(colors[i]);
+                cui.rect(margin + (swatchSize + margin) * (i % 4), margin * 2 + imgHeight + (i / 4) * (swatchSize + margin), swatchSize, swatchSize);
+            }
+
+            cui.endDraw();
+            app.image(cui, margin, margin);
         }
-
-        for (int i = 0; i < numColors; i++) {
-            cui.fill(colors[i]);
-            cui.rect(margin + (swatchSize + margin) * (i % 4), margin * 2 + imgHeight + (i / 4) * (swatchSize + margin), swatchSize, swatchSize);
-        }
-
-        cui.endDraw();
-        app.image(cui,margin,margin);
     }
 
-    public Boolean isVisible(){
+    public Boolean isVisible() {
 
         return isVisible;
     }
 
-    public int getSwatchSize(){
+    public int getSwatchSize() {
         return swatchSize;
     }
-    public float getGradientPosition(){
+
+    public float getGradientPosition() {
         return gradientPosition;
     }
-    public void setGradientPosition(float _gp){
+
+    public void setGradientPosition(float _gp) {
         gradientPosition = _gp;
     }
-    public int getColor(int _whichColor){
+
+    public int getColor(int _whichColor) {
 
         return colors[_whichColor];
+    }
+
+    public void toggleVisibility() {
+        isVisible = !isVisible;
+        cp5.setVisible(!cp5.isVisible());
+    }
+
+    public void playPause() {
+        gAutoScrollToggle.toggle();
     }
 
 
