94c604c9becceff6419f416bad152fc9a7b45a35
Cameron Browning <camb416@gmail.com>
2019-08-09 14:53:48 -0400

    basic flow field calculation without diagonals

--- tools/flow-generator/src/App.java
+++ tools/flow-generator/src/App.java
@@ -8,7 +8,6 @@ public class App extends PApplet {
     PVector field[][];
 
 
-
     public static void main(String[] args) {
         PApplet.main("App", args);
     }
@@ -20,11 +19,35 @@ public class App extends PApplet {
 
     public void setup() {
         img = loadImage("tools/flow-generator/noise.png");
-
+        img.loadPixels();
+        int width = img.width;
+        int height = img.height;
         field = new PVector[128][72];
-        for(int i = 0; i<128;i++){
+        for (int i = 0; i < 128; i++) {
             // field[i] = new PVector[72];
-        for(int j=0;j<72;j++) field[i][j] = new PVector();
+            for (int j = 0; j < 72; j++) {
+
+                float thisx = 0.0f;
+                float thisy = 0.0f;
+                if(i>0 && j>0 && i<(width-1) && j<(height-1)){
+
+
+                int p = j * width + i;
+                int nw = p - width - 1;
+                int n = p - width;
+                int ne = p - width + 1;
+                int w = p - 1;
+                int e = p + 1;
+                int sw = p + width - 1;
+                int s = p + width;
+                int se = p + width + 1;
+
+                thisx = (float) (red(img.pixels[e]) - red(img.pixels[w]))/255.0f;
+                thisy = (float) (red(img.pixels[s]) - red(img.pixels[n]))/255.0f;
+                }
+                field[i][j] = new PVector(thisx,thisy);
+
+            }
         }
     }
 
@@ -50,12 +73,12 @@ public class App extends PApplet {
         fill(255);
         image(img, 0, 0, 1280, 720);
 
-        stroke(255,0,0);
+        stroke(255, 0, 0);
         noFill();
-        for(int i = 0; i<128;i++){
+        for (int i = 0; i < 128; i++) {
             // field[i] = new PVector[72];
-            for(int j=0;j<72;j++){
-                line(i*10,j*10,i*10 + field[i][j].x*10, j*10 + field[i][j].y*10 );
+            for (int j = 0; j < 72; j++) {
+                line(i * 10, j * 10, i * 10 + field[i][j].x * 20.0f, j * 10 + field[i][j].y * 20.0f);
             }
         }
 
